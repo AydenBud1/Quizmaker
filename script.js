0c5d81208679591aedf7db6a484de39cc35a3f19
Ayden Budish <ab44990@ALA.local>
2023-01-25 08:32:15 -0700

    hel

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  var time = 90
+  function timer() {
+    console.log ("working")
+  }
+ setInterval(timer,1000)
+    
+
+ function correctAnswer(){
+  console.log("working")
+ }
